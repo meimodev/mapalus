9a7581853d0b956f00f2f8e1fcec399e35a3280a
meimo <meimodev@gmail.com>
2022-04-20 03:36:48 +0800

    finished working on user flow when completing an order, working order detail

--- lib/shared/utils.dart
+++ lib/shared/utils.dart
@@ -2,7 +2,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:intl/intl.dart';
 import 'dart:math';
 
-
 class Utils {
   static String formatNumberToCurrency(dynamic number) {
     var f = NumberFormat('#,###');
